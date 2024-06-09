07d97e898a43832e1ff78b80e1d767980987b1f0
DictumMortuum <theio.vrefos@gmail.com>
2024-06-09 17:56:47 +0300

    [extapi] added email on the participation

--- pkg/model/eurovisionparticipations.go
+++ pkg/model/eurovisionparticipations.go
@@ -10,6 +10,7 @@ import (
 type EurovisionParticipation struct {
 	Id          int64     `gorm:"primaryKey" json:"id,omitempty"`
 	UserId      string    `json:"user_id,omitempty"`
+	Email       string    `json:"email"`
 	BoardgameId int64     `json:"boardgame_id"`
 	Boardgame   Boardgame `json:"boardgame"`
 }
