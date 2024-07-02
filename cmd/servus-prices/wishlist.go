398a6e7b7ffd83eb62cfb4efa5c96069c16ac3c8
DictumMortuum <theio.vrefos@gmail.com>
2024-07-02 22:39:11 +0300

    [prices] removing wishlist items

--- cmd/servus-prices/wishlist.go
+++ cmd/servus-prices/wishlist.go
@@ -1,61 +1,58 @@
 package main
 
-import (
-	"strings"
+// import (
+// 	"strings"
 
-	"github.com/DictumMortuum/servus-extapi/pkg/bgg"
-	"github.com/DictumMortuum/servus-extapi/pkg/model"
-	"github.com/gin-gonic/gin"
-	"github.com/mrz1836/go-sanitize"
-	"gorm.io/gorm"
-)
+// 	"github.com/mrz1836/go-sanitize"
+// 	"gorm.io/gorm"
+// )
 
-func addSearch(val string) func(*gorm.DB) *gorm.DB {
-	terms := strings.Split(sanitize.AlphaNumeric(val, true), " ")
+// func addSearch(val string) func(*gorm.DB) *gorm.DB {
+// 	terms := strings.Split(sanitize.AlphaNumeric(val, true), " ")
 
-	return func(db *gorm.DB) *gorm.DB {
-		for _, term := range terms {
-			term = "%" + term + "%"
-			db = db.Where("name COLLATE utf8mb4_unicode_ci LIKE ?", term)
-		}
+// 	return func(db *gorm.DB) *gorm.DB {
+// 		for _, term := range terms {
+// 			term = "%" + term + "%"
+// 			db = db.Where("name COLLATE utf8mb4_unicode_ci LIKE ?", term)
+// 		}
 
-		return db
-	}
-}
+// 		return db
+// 	}
+// }
 
-func wishlistFilter(c *gin.Context) {
-	username := c.Query("username")
+// func wishlistFilter(c *gin.Context) {
+// 	username := c.Query("username")
 
-	m, err := model.ToMap(c, "req")
-	if err != nil {
-		c.Error(err)
-		return
-	}
+// 	m, err := model.ToMap(c, "req")
+// 	if err != nil {
+// 		c.Error(err)
+// 		return
+// 	}
 
-	db, err := m.GetGorm()
-	if err != nil {
-		c.Error(err)
-		return
-	}
+// 	db, err := m.GetGorm()
+// 	if err != nil {
+// 		c.Error(err)
+// 		return
+// 	}
 
-	if username != "" {
-		col, err := bgg.Wishlist("Dictum Mortuum")
-		if err != nil {
-			c.Error(err)
-			return
-		}
+// 	if username != "" {
+// 		col, err := bgg.Wishlist("Dictum Mortuum")
+// 		if err != nil {
+// 			c.Error(err)
+// 			return
+// 		}
 
-		for i, item := range col.Items {
-			tx := db.Session(&gorm.Session{NewDB: true}).Unscoped()
+// 		for i, item := range col.Items {
+// 			tx := db.Session(&gorm.Session{NewDB: true}).Unscoped()
 
-			if i == 0 {
-				db = db.Where(tx.Scopes(addSearch(item.Name)))
-			} else {
-				db = db.Or(tx.Scopes(addSearch(item.Name)))
-			}
-		}
+// 			if i == 0 {
+// 				db = db.Where(tx.Scopes(addSearch(item.Name)))
+// 			} else {
+// 				db = db.Or(tx.Scopes(addSearch(item.Name)))
+// 			}
+// 		}
 
-		db = db.Debug()
-		m.Set("gorm", db)
-	}
-}
+// 		db = db.Debug()
+// 		m.Set("gorm", db)
+// 	}
+// }
