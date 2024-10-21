788141aacf2e098673bb4c753a102a7e365dd9aa
DictumMortuum <theio.vrefos@gmail.com>
2024-10-21 19:25:15 +0300

    [deco] using Device from the model package

--- cmd/servus-deco/db.go
+++ cmd/servus-deco/db.go
@@ -2,22 +2,17 @@ package main
 
 import (
 	"github.com/DictumMortuum/servus-extapi/pkg/db"
+	"github.com/DictumMortuum/servus-extapi/pkg/model"
 )
 
-type Mapping struct {
-	Id    int64  `json:"id"`
-	Mac   string `json:"mac"`
-	Alias string `json:"alias"`
-}
-
-func getMappings() ([]Mapping, error) {
+func getMappings() ([]model.Device, error) {
 	DB, err := db.DatabaseX()
 	if err != nil {
 		return nil, err
 	}
 	defer DB.Close()
 
-	rs := []Mapping{}
+	rs := []model.Device{}
 	q := `select id, mac, alias from tdevices`
 	err = DB.Select(&rs, q)
 	if err != nil {
